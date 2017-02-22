a4f346bef41ba096d8fdf674bd8fdb9e253fde8d
Shuhei Takahashi <dobby.dog.doughnut@gmail.com>
2017-02-22 19:01:45 +0900

    refactor: add d.ts generation to prepublish script

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Bluebird from 'bluebird';
-import { SftpSyncConfig, SftpSyncOptions } from './lib/config';
-
-export { SftpSync } from './lib/sftpSync';
-export { SftpSyncConfig, SftpSyncOptions };
-
-export function deploy(config: SftpSyncConfig, options?: SftpSyncOptions): Bluebird<void>;
-
-export default deploy;
