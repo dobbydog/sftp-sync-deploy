791bfc43884e2348d0c8106f6692772dd0c30639
Shuhei Takahashi <dobby.dog.doughnut@gmail.com>
2017-02-22 18:56:18 +0900

    refactor: index.js -> index.ts

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as Bluebird from 'bluebird';
+import { SftpSyncConfig, SftpSyncOptions } from './lib/config';
+import { SftpSync } from './lib/sftpSync';
+
+export function deploy(config: SftpSyncConfig, options?: SftpSyncOptions): Bluebird<void> {
+  const deployer = new SftpSync(config, options);
+
+  console.log(`* Deploying to host ${config.host}`.green);
+  console.log('* local dir  = '.gray + deployer.localRoot);
+  console.log('* remote dir = '.gray + deployer.remoteRoot);
+  console.log('');
+
+  return deployer.sync();
+};
+
+export default deploy;
+export * from './lib/SftpSync';
+export * from './lib/config';
